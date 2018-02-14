cb18e7c2e49c9ea55745d990e42ac8a9ba2a5e07
Kind <Kind@WoZiPc.fritz.box>
2018-02-14 18:36:24 +0100

    save

--- Version 0.0.1/src/classes/Hexagon.java
+++ Version 0.0.1/src/classes/Hexagon.java
@@ -8,16 +8,15 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
 public class Hexagon extends Polygon {
-	
+
 	private Lighting l;
 	private Light.Distant li;
 
 	public Hexagon(double x, double y, double r) {
 
-		getPoints().addAll((x + Math.sin(0) * r), (y + Math.cos(0) * r), 
-				(x + Math.sin(Math.PI / 3) * r),(y + Math.cos(Math.PI / 3) * r),
-				(x + Math.sin(Math.PI / 3 * 2) * r),(y + Math.cos(Math.PI / 3 * 2) * r), 
-				(x + Math.sin(Math.PI) * r), (y + Math.cos(Math.PI) * r),
+		getPoints().addAll((x + Math.sin(0) * r), (y + Math.cos(0) * r), (x + Math.sin(Math.PI / 3) * r),
+				(y + Math.cos(Math.PI / 3) * r), (x + Math.sin(Math.PI / 3 * 2) * r),
+				(y + Math.cos(Math.PI / 3 * 2) * r), (x + Math.sin(Math.PI) * r), (y + Math.cos(Math.PI) * r),
 				(x + Math.sin(Math.PI / 3 * 4) * r), (y + Math.cos(Math.PI / 3 * 4) * r),
 				(x + Math.sin(Math.PI / 3 * 5) * r), (y + Math.cos(Math.PI / 3 * 5) * r));
 
@@ -32,46 +31,47 @@ public class Hexagon extends Polygon {
 		setEffect(l);
 
 		setFill(Color.STEELBLUE);
-		
-		
-		setOnMouseEntered(new EventHandler<MouseEvent>(){
+
+		setOnMouseEntered(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent arg0) {
 				setFill(Color.DARKGOLDENROD);
-				}
-			
+			}
+
 		});
-		
-		setOnMouseExited(new EventHandler<MouseEvent>(){
+
+		setOnMouseExited(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent arg0) {
 				setFill(Color.STEELBLUE);
-				}
-			
+			}
+
 		});
-		
+
 		setOnMousePressed(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
 				li.setAzimuth(45);
 				setEffect(l);
-				
+
 			}
-			
+
 		});
-		
+
 		setOnMouseReleased(new EventHandler<MouseEvent>() {
 
 			@Override
 			public void handle(MouseEvent event) {
 				li.setAzimuth(-135);
+				li.setColor(Color.WHITESMOKE);
 				setEffect(l);
 				
+
 			}
-			
+
 		});
 
 	}
