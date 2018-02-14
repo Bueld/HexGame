6aeaccb89f4c2a7715f2b10bda3ee6f9283df773
Kind <Kind@WoZiPc.fritz.box>
2018-02-14 17:57:08 +0100

    Finally version 0.0.2

--- Version 0.0.1/src/classes/Frame.java
+++ Version 0.0.1/src/classes/Frame.java
@@ -1,10 +1,57 @@
 package classes;
 
-public class Frame {
+import java.util.ArrayList;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Frame extends Application {
+	
+	private ArrayList<Hexagon> hexs = new ArrayList<Hexagon>();
+	
+	private Group hGroup;
+	
+	private double radius = 50;
+
+	@Override
+	public void init() {
+		
+		double a = Math.sqrt(3);
+		
+		for (int i = 0;i<20;i++) {
+			for (int j = 0;j<12;j+=2) {
+				hexs.add(new Hexagon(radius+(i*Math.sqrt(3)*radius),((j)*(radius+Math.sin(Math.PI/6)*radius)+radius),radius));
+				hexs.add(new Hexagon(radius+Math.sqrt(3)*radius/2+(i*Math.sqrt(3)*radius),((j+1)*(radius+Math.sin(Math.PI/6)*radius)+radius),radius));
+			}
+		}
+		
+		
+	}
+
+	@Override
+	public void start(final Stage stage) {
+		
+		hGroup = new Group();
+		hGroup.getChildren().addAll(hexs);
+		
+		Pane pane = new Pane();
+		pane.getChildren().addAll(hGroup);
+
+		Scene scene = new Scene(pane);
+		scene.setFill(Color.rgb(30, 6, 40));
+
+		stage.setScene(scene);
+		stage.setFullScreen(true);
+		stage.setFullScreenExitHint("Press ECS to exit Fullscreen");
+		stage.show();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 }
--- Version 0.0.1/src/classes/Hexagon.java
+++ Version 0.0.1/src/classes/Hexagon.java
@@ -1,5 +1,78 @@
 package classes;
 
-public class Hexagon {
+import javafx.event.EventHandler;
+import javafx.scene.effect.Light;
+import javafx.scene.effect.Lighting;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+public class Hexagon extends Polygon {
+	
+	private Lighting l;
+	private Light.Distant li;
+
+	public Hexagon(double x, double y, double r) {
+
+		getPoints().addAll((x + Math.sin(0) * r), (y + Math.cos(0) * r), 
+				(x + Math.sin(Math.PI / 3) * r),(y + Math.cos(Math.PI / 3) * r),
+				(x + Math.sin(Math.PI / 3 * 2) * r),(y + Math.cos(Math.PI / 3 * 2) * r), 
+				(x + Math.sin(Math.PI) * r), (y + Math.cos(Math.PI) * r),
+				(x + Math.sin(Math.PI / 3 * 4) * r), (y + Math.cos(Math.PI / 3 * 4) * r),
+				(x + Math.sin(Math.PI / 3 * 5) * r), (y + Math.cos(Math.PI / 3 * 5) * r));
+
+		l = new Lighting();
+		li = new Light.Distant();
+		li.setAzimuth(-135);
+		li.setColor(Color.WHITESMOKE);
+		l.setLight(li);
+		l.setSurfaceScale(3.0);
+
+		setEffect(l);
+
+		setFill(Color.STEELBLUE);
+		
+		
+		setOnMouseEntered(new EventHandler<MouseEvent>(){
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				setFill(Color.DARKGOLDENROD);
+				}
+			
+		});
+		
+		setOnMouseExited(new EventHandler<MouseEvent>(){
+
+			@Override
+			public void handle(MouseEvent arg0) {
+				setFill(Color.STEELBLUE);
+				}
+			
+		});
+		
+		setOnMousePressed(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				li.setAzimuth(45);
+				setEffect(l);
+				
+			}
+			
+		});
+		
+		setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				li.setAzimuth(-135);
+				setEffect(l);
+				
+			}
+			
+		});
+
+	}
 
 }
